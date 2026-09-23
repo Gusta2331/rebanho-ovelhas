5dbb83002ee8c6a1fed2f8d463a0f6ec5246604e
Gustavo Santana <gustavosanmar2020@gmail.com>
2026-09-22 21:12:40 -0300

    feat: iniciar sincronizacao offline no app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app.dart';
 import 'core/config/supabase_config.dart';
+import 'core/offline/offline_sync_service.dart';
 import 'core/services/supabase_connection_test.dart';
 
 Future<void> main() async {
@@ -12,6 +13,8 @@ Future<void> main() async {
     url: SupabaseConfig.url,
     publishableKey: SupabaseConfig.publishableKey,
   );
+
+  await OfflineSyncService.instance.iniciar();
 
   final conectado = await SupabaseConnectionTest.testar();
 
